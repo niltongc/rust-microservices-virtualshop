e3818d398fb634f57a5a9fb18badab8cc85bfbbb
nilton <ngcquimica@gmail.com>
2026-01-20 13:54:46 -0300

    feat: host and port added dinamically

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::env;
+
 use actix_web::{App, HttpServer};
+use dotenvy::dotenv;
 
 use crate::route_config::route_config;
 
@@ -8,13 +11,23 @@ mod handlers;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
-    println!("running at localhost:8080");
+    dotenv().ok();
+
+    let host = env::var("HOST")
+        .expect("HOST not found");
+
+    let port: u16 = env::var("PORT")
+        .expect("PORT not found")
+        .parse()
+        .expect("PORT must be a number");
+
+    println!("running at {}:{}",host,port);
 
     HttpServer::new(|| {
         App::new()
             .configure(route_config)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((host.to_string(), port))?
     .run()
     .await
 }
